79851d78ffcdd4f779a76786c84b314f1b1ca9f0
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-07-13 14:56:37 -0700

    CSS: add/style drop down details section

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Card.module.css';
 
 export default function Card() {
+
+  const [viewDeatils, setViewDetials] = useState(true);
 
   const bgImage = {
     backgroundImage: `url(${data.img})`,
   };
 
   return (
-    <li className={styles.card}>
+    <li className={`${styles.card } ${!viewDeatils? '' : styles.expand}`}>
 
       <div className={ styles.img } style={ bgImage }/>
       <div className={ styles.info }>
         <h3>{ data.name }</h3>
-        <p>{ data.saved }</p>
+        <p>${ data.saved } of ${ data.total }</p>
         <p>{ data.status }</p>
       </div>
       <div className={ styles.progress }>
         { data.percentSaved }
       </div>
-      <div className={ styles.details }></div>
-      <button className={ styles.toggle }>View details</button>
+      <div 
+        className={`${styles.details } ${!viewDeatils? '' : styles.expand}`}>
+        <p><span>Start date</span><span>{ data.started }</span></p>    
+        <p><span>Estimated finsh date</span><span>{ data.started }</span></p>    
+        <p><span>$ per day</span><span>{ data.perDay }</span></p>    
+        <p><span>Days left</span><span>{ 12 }</span></p>  
+        <button>Save faster</button>  
+        <button>Pause</button>  
+      </div>
+      <button 
+        onClick={() => setViewDetials(!viewDeatils)}
+        className={ styles.toggle }>
+        {viewDeatils ? 'Hide details' : 'View details'}
+      </button>
     </li>
   )
 }
