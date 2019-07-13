229e765c0c50e39a7d77d96d32c9f410a7a38906
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-07-13 14:21:43 -0700

    CSS: add grid layout to Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,9 +2,37 @@ import React from 'react';
 import styles from './Card.module.css';
 
 export default function Card() {
+
+  const bgImage = {
+    backgroundImage: `url(${data.img})`,
+  };
+
   return (
     <li className={styles.card}>
-      Card
+
+      <div className={ styles.img } style={ bgImage }/>
+      <div className={ styles.info }>
+        <h3>{ data.name }</h3>
+        <p>{ data.saved }</p>
+        <p>{ data.status }</p>
+      </div>
+      <div className={ styles.progress }>
+        { data.percentSaved }
+      </div>
+      <div className={ styles.details }></div>
+      <button className={ styles.toggle }>View details</button>
     </li>
   )
+}
+
+
+const data = {
+  img: 'http://www.pngmart.com/files/3/BMW-M3-Transparent-Background.png',
+  name: 'BMW 328ci',
+  status: 'active',
+  total: 35000,
+  started: '2017-5-1',
+  perDay: 15,
+  saved: 12000,
+  percentSaved: 34,
 }
