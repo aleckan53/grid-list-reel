8398e8a3e70e8d51a3186ded6010d326f305cbbd
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-07-13 16:55:37 -0700

    add Filter component

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Filter.module.css';
+
+export default function Filter() {
+
+  return (
+    <div className={styles.filter}>
+    </div>
+  )
+}
---
+++ src/components/Filter/Filter.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Filter from './Filter'
+
+describe('Filter component', () => {
+  it('matches the snapshot', () => {
+    const root = document.createElement('div');
+    ReactDOM.render(<Filter/>, root);
+    expect(root).toMatchSnapshot();
+    ReactDOM.unmountComponentAtNode(root);
+  });
+});
