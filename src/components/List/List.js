4b95af771de4907aa5fc888d33e632aff55b6886
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-07-13 15:48:26 -0700

    add List component

---
+++ src/components/List/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './List.module.css';
+
+export default function List() {
+
+  return (
+    <ul className={styles.list}>
+      
+    </ul>
+  )
+}
---
+++ src/components/List/List.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import List from './List';
+
+describe('List component', () => {
+
+  it('fetches and renders list of Cards', () => {
+    
+  })
+})
