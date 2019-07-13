beb941f4fc3eb09a026938daa7bbe106f5fdaa94
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-07-13 13:31:51 -0700

    add basic global styles

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Card from './Card';
 
 describe('Card component', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Card />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
+  let root = document.createElement('div');
+
+  beforeEach(() => {
+    ReactDOM.render(<Card />, root);
+  })
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(root);
+  })
+
+  it('matches the snapshot', () => {
+    expect(root).toMatchSnapshot();
+  })
     
 });
