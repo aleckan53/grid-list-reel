f6261a2b6e80e63a88e53692a70a98c198b93ab0
aleckan53 <admin@admins-MacBook-Pro-3.local>
2019-07-13 13:34:45 -0700

    update css variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './variables.css';
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
